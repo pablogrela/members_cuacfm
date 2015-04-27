363c1632c8c76518675ab16e506ead04a57e3bca
pablogrela <pablogp_9@hotmail.com>
2015-04-27 21:58:39 +0200

    Actualizar el perfil con las especificaciones de cuacfm
    0.0.9-SNAPSHOT- improve accounts, training, js, payments
    
    1.Se modifico la vista profile con el controlador y el formulario para
    añadir los nuevos campos requeridos por cuacfm (dni, address, mobile,
    phone (opcional), programName(opcional), student y datebirth).
    
    2.Se añadieron las restricciones unique a dni, login y email.

--- src/main/java/org/cuacfm/members/web/profile/ProfileController.java
+++ src/main/java/org/cuacfm/members/web/profile/ProfileController.java
@@ -6,7 +6,11 @@ import javax.validation.Valid;
 
 import org.cuacfm.members.model.account.Account;
 import org.cuacfm.members.model.accountService.AccountService;
+import org.cuacfm.members.model.accountTypeService.AccountTypeService;
+import org.cuacfm.members.model.exceptions.UniqueException;
+import org.cuacfm.members.model.methodPaymentService.MethodPaymentService;
 import org.cuacfm.members.model.userService.UserService;
+import org.cuacfm.members.web.support.DisplayDate;
 import org.cuacfm.members.web.support.MessageHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -31,9 +35,17 @@ public class ProfileController {
 	/** The user service. */
 	@Autowired
 	private UserService userService;
+	
+	/** The account Type service. */
+	@Autowired
+	private AccountTypeService accountTypeService;
+	
+	/** The account Method Payment service. */
+	@Autowired
+	private MethodPaymentService methodPaymentService;
 
 	/** The Global variable account. */
-	Account account;
+	private Account account = null;
 
 	/**
 	 * Instantiates a new Profile controller.
@@ -54,24 +66,26 @@ public class ProfileController {
 	@RequestMapping(value = "profile")
 	public String profile(Model model, Principal principal) {
 
-		// Show info about user
 		account = accountService.findByLogin(principal.getName());
-
+			
+		// Show info about user
 		ProfileForm profileForm = new ProfileForm();
-
 		profileForm.setName(account.getName());
+		profileForm.setDni(account.getDni());
+		profileForm.setAddress(account.getAddress());
 		profileForm.setLogin(account.getLogin());
 		profileForm.setEmail(account.getEmail());
+		profileForm.setPhone(account.getPhone());
+		profileForm.setMobile(account.getMobile());
+		profileForm.setProgramName(account.getProgramName());
+		profileForm.setStudent(account.isStudent());
+		profileForm.setDateBirth(DisplayDate.dateToString(account.getDateBirth()));
 		model.addAttribute(profileForm);
 
-		/*
-		 * model.addAttribute(new ProfileForm()); model.addAttribute("name",
-		 * account.getName()); model.addAttribute("login", account.getLogin());
-		 * model.addAttribute("email", account.getEmail());
-		 */
 		return PROFILE_VIEW_NAME;
 	}
-
+	
+	
 	/**
 	 * Profile.
 	 *
@@ -92,35 +106,44 @@ public class ProfileController {
 			Errors errors, RedirectAttributes ra, Principal principal,
 			Model model) {
 
-		// if (account != null) {
-		// profileForm.setName(account.getName());
-		// profileForm.setLogin(account.getLogin());
-		// profileForm.setEmail(account.getEmail());
-		// }
-
-		// Show info about user
-		// Account account = accountService.findByLogin(principal.getName());
-		// model.addAttribute("name", account.getName());
-		// model.addAttribute("login", account.getLogin());
-		// model.addAttribute("email", account.getEmail());
-		// model.addAttribute("role", account.getRole());
-
+	
+		if (errors.hasErrors()) {
+			return PROFILE_VIEW_NAME;
+		}
+		
 		boolean modify = false;
+		
+		// Name
 		String name = profileForm.getName();
-		if (name != null && name != "") {
+		if (profileForm.isOnName() && name != "") {
 			account.setName(name);
+			modify = true;
+		}
+
+		// Dni
+		String dni = profileForm.getDni();
+		if (profileForm.isOnDni() && dni != "") {
+			account.setDni(dni);
+			modify = true;
+		}
+		
+		// Adrress
+		String address = profileForm.getAddress();
+		if (profileForm.isOnAddress() && address != "") {
+			account.setAddress(address);
 			modify = true;
 		}
 
 		// Password
 		boolean modifyPassword = false;
-		if (profileForm.getPassword() != null) {
+		if (profileForm.isOnPassword()) {
 			// check that the password and rePassword are the same
 			String password = profileForm.getPassword();
 			String rePassword = profileForm.getRePassword();
 			if (!password.equals(rePassword)) {
 				errors.rejectValue("password", "profile.passwordsDontMatch");
 				errors.rejectValue("rePassword", "profile.passwordsDontMatch");
+				return PROFILE_VIEW_NAME;
 			} else {
 				account.setPassword(password);
 				modify = true;
@@ -128,59 +151,84 @@ public class ProfileController {
 			}
 		}
 
-		if (errors.hasErrors()) {
-			return PROFILE_VIEW_NAME;
-		}
-
-		// Queries to database
-		// Login
-		Account accountSearch;
+		// Login, Queries to database
 		boolean modifyLogin = false;
 		String login = profileForm.getLogin();
-		if (login != null && login != "") {
+		if (profileForm.isOnLogin() && login != "") {
 			// check that doesn't exist other login
-			accountSearch = accountService.findByLogin(login);
-			if (accountSearch != null) {
-				if (accountSearch.getId() != account.getId()) {
-					errors.rejectValue("login", "profile.existentLogin",
-							new Object[] { login }, "login");
-				}
-			} else {
-				account.setLogin(login);
-				modify = true;
-				modifyLogin = true;
-			}
+			account.setLogin(login);
+			modify = true;
+			modifyLogin = true;	
 		}
 
 		// Email
 		String email = profileForm.getEmail();
-		if (email != null && email != "") {
+		if (profileForm.isOnEmail() && email != "") {
 			// check that doesn't exist other email
-			accountSearch = accountService.findByEmail(email);
-			if (accountSearch != null) {
-				if (accountSearch.getId() != account.getId()) {
+			account.setEmail(profileForm.getEmail());
+			modify = true;		
+		}
+
+		// Phone
+		if (profileForm.isOnPhone()){
+			account.setPhone(profileForm.getPhone());
+			modify = true;
+		}
+		
+		// Mobile
+		if (profileForm.isOnMobile()){
+			account.setMobile(profileForm.getMobile());
+			modify = true;
+		}
+		
+		// ProgramName
+		String programName = profileForm.getProgramName();
+		if (profileForm.isOnProgramName() && programName != "") {
+			account.setProgramName(programName);
+			modify = true;
+		}
+		
+		// Student
+		if (profileForm.isOnStudent()){
+			account.setStudent(profileForm.isStudent());
+			modify = true;
+		}
+
+		// DateBirth
+		if (profileForm.isOnDateBirth()){
+			account.setDateBirth(DisplayDate.stringToDate2(profileForm.getDateBirth()));
+			modify = true;
+		}
+		
+		// If correct
+		if (modify) {
+			try {
+				accountService.update(account, modifyPassword);
+			} catch (UniqueException e) {
+				if (e.getAttribute() == "Dni") {
+					errors.rejectValue("dni", "signup.existentDni",
+							new Object[] { dni }, "dni");
+				}
+				if (e.getAttribute() == "Login") {
+					errors.rejectValue("login", "profile.existentLogin",
+							new Object[] { login }, "login");
+				}
+				if (e.getAttribute() == "Email") {
 					errors.rejectValue("email", "profile.existentEmail",
 							new Object[] { email }, "email");
 				}
-			} else {
-				account.setEmail(email);
-				modify = true;
 			}
-		}
-
-		if (errors.hasErrors()) {
-			return PROFILE_VIEW_NAME;
-		}
-
-		// If correct
-		if (modify == true) {
-
-			accountService.update(account, modifyPassword);
-			if (modifyLogin == true) {
+			
+			if (errors.hasErrors()) {
+				return PROFILE_VIEW_NAME;
+			}
+			
+			if (modifyLogin) {
 				userService.signin(account);
 			}
 			MessageHelper.addSuccessAttribute(ra, "profile.success");
 		}
+		
 		return "redirect:/profile";
 	}
 
--- src/main/java/org/cuacfm/members/web/profile/ProfileForm.java
+++ src/main/java/org/cuacfm/members/web/profile/ProfileForm.java
@@ -1,22 +1,39 @@
 package org.cuacfm.members.web.profile;
 
+import java.util.List;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.Size;
+
+import org.cuacfm.members.model.account.Account;
+import org.cuacfm.members.model.accountType.AccountType;
+import org.cuacfm.members.model.methodPayment.MethodPayment;
 import org.hibernate.validator.constraints.Email;
 
+/** The Class ProfileForm. */
 public class ProfileForm {
 
-	/** The Constant NOT_BLANK_MESSAGE. */
-	// private static final String NOT_BLANK_MESSAGE = "{notBlank.message}";
 	/** The Constant EMAIL_MESSAGE. */
 	private static final String EMAIL_MESSAGE = "{email.message}";
+
 	/** The Constant INSUFFICIENT_CHARACTERS. */
 	private static final String INSUFFICIENT_CHARACTERS = "{insuficient.characters}";
+
 	/** The Constant MAX_CHARACTERS. */
 	private static final String MAX_CHARACTERS = "{max.characters}";
 
 	/** The name. */
 	@Size(max = 30, message = ProfileForm.MAX_CHARACTERS)
 	private String name;
+
+	/** The dni. */
+	@Size(max = 30, message = ProfileForm.MAX_CHARACTERS)
+	private String dni;
+
+	/** The address. */
+	@Size(max = 30, message = ProfileForm.MAX_CHARACTERS)
+	private String address;
 
 	/** The login. */
 	@Size(max = 30, message = ProfileForm.MAX_CHARACTERS)
@@ -27,6 +44,24 @@ public class ProfileForm {
 	@Size(max = 30, message = ProfileForm.MAX_CHARACTERS)
 	private String email;
 
+	/** The phone. */
+	// @Size(max = 30, message = SignupForm.MAX_CHARACTERS)
+	private int phone;
+
+	/** The mobile. */
+	// @Size(max = 30, message = SignupForm.MAX_CHARACTERS)
+	private int mobile;
+
+	/** The program name. */
+	@Size(max = 30, message = ProfileForm.MAX_CHARACTERS)
+	private String programName;
+
+	/** The student. */
+	private boolean student;
+
+	/** The date birth. */
+	private String dateBirth;
+
 	/** The password. */
 	@Size(min = 4, max = 20, message = ProfileForm.INSUFFICIENT_CHARACTERS)
 	private String password;
@@ -35,30 +70,86 @@ public class ProfileForm {
 	@Size(min = 4, max = 20, message = ProfileForm.INSUFFICIENT_CHARACTERS)
 	private String rePassword;
 
+	/** The accountType List. */
+	private List<AccountType> accountTypes;
+
+	/** The Global variable accountTypeId. */
+	private Long accountTypeId;
+
+	/** The methodPayment List. */
+	private List<MethodPayment> methodPayments;
+
+	/** The Global variable methodPaymentId. */
+	private Long methodPaymentId;
+
+	/** The installments. */
+	@Min(0)
+	@Max(12)
+	private int installments;
+
+	/** The observations. */
+	@Size(max = 500, message = ProfileForm.MAX_CHARACTERS)
+	private String observations;
+
+	/** The roles. */
+	private List<Account.roles> roles;
+
+	/** The role. */
+	private String role;
+
+	/** The on name. */
+	private boolean onName;
+
+	/** The on dni. */
+	private boolean onDni;
+
+	/** The on address. */
+	private boolean onAddress;
+
+	/** The on login. */
+	private boolean onLogin;
+
+	/** The on email. */
+	private boolean onEmail;
+
+	/** The on phone. */
+	private boolean onPhone;
+
+	/** The on mobile. */
+	private boolean onMobile;
+
+	/** The on student. */
+	private boolean onStudent;
+
+	/** The on ProgramName. */
+	private boolean onProgramName;
+
+	/** The on date birth. */
+	private boolean onDateBirth;
+
+	/** The on password. */
+	private boolean onPassword;
+
+	/** The on account type. */
+	private boolean onAccountType;
+
+	/** The on method payment. */
+	private boolean onMethodPayment;
+
+	/** The on installment. */
+	private boolean onInstallments;
+
+	/** The on observations. */
+	private boolean onObservations;
+
+	/** The on role. */
+	private boolean onRole;
+
 	/**
 	 * Instantiates a new profile form.
 	 */
 	public ProfileForm() {
 		// Default empty constructor.
-	}
-
-	/**
-	 * Gets the login.
-	 *
-	 * @return the login
-	 */
-	public String getLogin() {
-		return login;
-	}
-
-	/**
-	 * Sets the login.
-	 *
-	 * @param login
-	 *            the new login
-	 */
-	public void setLogin(String login) {
-		this.login = login;
 	}
 
 	/**
@@ -81,6 +172,63 @@ public class ProfileForm {
 	}
 
 	/**
+	 * Gets the dni.
+	 *
+	 * @return the dni
+	 */
+	public String getDni() {
+		return dni;
+	}
+
+	/**
+	 * Sets the dni.
+	 *
+	 * @param dni
+	 *            the new dni
+	 */
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	/**
+	 * Gets the address.
+	 *
+	 * @return the address
+	 */
+	public String getAddress() {
+		return address;
+	}
+
+	/**
+	 * Sets the address.
+	 *
+	 * @param address
+	 *            the new address
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	/**
+	 * Gets the login.
+	 *
+	 * @return the login
+	 */
+	public String getLogin() {
+		return login;
+	}
+
+	/**
+	 * Sets the login.
+	 *
+	 * @param login
+	 *            the new login
+	 */
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	/**
 	 * Gets the email.
 	 *
 	 * @return the email
@@ -97,6 +245,101 @@ public class ProfileForm {
 	 */
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	/**
+	 * Gets the phone.
+	 *
+	 * @return the phone
+	 */
+	public int getPhone() {
+		return phone;
+	}
+
+	/**
+	 * Sets the phone.
+	 *
+	 * @param phone
+	 *            the new phone
+	 */
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+
+	/**
+	 * Gets the mobile.
+	 *
+	 * @return the mobile
+	 */
+	public int getMobile() {
+		return mobile;
+	}
+
+	/**
+	 * Sets the mobile.
+	 *
+	 * @param mobile
+	 *            the new mobile
+	 */
+	public void setMobile(int mobile) {
+		this.mobile = mobile;
+	}
+
+	/**
+	 * Gets the program name.
+	 *
+	 * @return the program name
+	 */
+	public String getProgramName() {
+		return programName;
+	}
+
+	/**
+	 * Sets the program name.
+	 *
+	 * @param programName
+	 *            the new program name
+	 */
+	public void setProgramName(String programName) {
+		this.programName = programName;
+	}
+
+	/**
+	 * Checks if is student.
+	 *
+	 * @return true, if is student
+	 */
+	public boolean isStudent() {
+		return student;
+	}
+
+	/**
+	 * Sets the student.
+	 *
+	 * @param student
+	 *            the new student
+	 */
+	public void setStudent(boolean student) {
+		this.student = student;
+	}
+
+	/**
+	 * Gets the date birth.
+	 *
+	 * @return the date birth
+	 */
+	public String getDateBirth() {
+		return dateBirth;
+	}
+
+	/**
+	 * Sets the date birth.
+	 *
+	 * @param dateBirth
+	 *            the new date birth
+	 */
+	public void setDateBirth(String dateBirth) {
+		this.dateBirth = dateBirth;
 	}
 
 	/**
@@ -119,21 +362,469 @@ public class ProfileForm {
 	}
 
 	/**
-	 * Gets the retry password.
+	 * Gets the re password.
 	 *
-	 * @return the retry password
+	 * @return the re password
 	 */
 	public String getRePassword() {
 		return rePassword;
 	}
 
 	/**
-	 * Sets the retry password.
+	 * Sets the re password.
 	 *
-	 * @param retry
-	 *            password the new retry password
+	 * @param rePassword
+	 *            the new re password
 	 */
 	public void setRePassword(String rePassword) {
 		this.rePassword = rePassword;
 	}
+
+	/**
+	 * Gets the account types.
+	 *
+	 * @return the account types
+	 */
+	public List<AccountType> getAccountTypes() {
+		return accountTypes;
+	}
+
+	/**
+	 * Sets the account types.
+	 *
+	 * @param accountTypes
+	 *            the new account types
+	 */
+	public void setAccountTypes(List<AccountType> accountTypes) {
+		this.accountTypes = accountTypes;
+	}
+
+	/**
+	 * Gets the account type id.
+	 *
+	 * @return the account type id
+	 */
+	public Long getAccountTypeId() {
+		return accountTypeId;
+	}
+
+	/**
+	 * Sets the account type id.
+	 *
+	 * @param accountTypeId
+	 *            the new account type id
+	 */
+	public void setAccountTypeId(Long accountTypeId) {
+		this.accountTypeId = accountTypeId;
+	}
+
+	/**
+	 * Gets the method payments.
+	 *
+	 * @return the method payments
+	 */
+	public List<MethodPayment> getMethodPayments() {
+		return methodPayments;
+	}
+
+	/**
+	 * Sets the method payments.
+	 *
+	 * @param methodPayments
+	 *            the new method payments
+	 */
+	public void setMethodPayments(List<MethodPayment> methodPayments) {
+		this.methodPayments = methodPayments;
+	}
+
+	/**
+	 * Gets the method payment id.
+	 *
+	 * @return the method payment id
+	 */
+	public Long getMethodPaymentId() {
+		return methodPaymentId;
+	}
+
+	/**
+	 * Sets the method payment id.
+	 *
+	 * @param methodPaymentId
+	 *            the new method payment id
+	 */
+	public void setMethodPaymentId(Long methodPaymentId) {
+		this.methodPaymentId = methodPaymentId;
+	}
+
+	/**
+	 * Gets the installments.
+	 *
+	 * @return the installments
+	 */
+	public int getInstallments() {
+		return installments;
+	}
+
+	/**
+	 * Sets the installments.
+	 *
+	 * @param installments
+	 *            the new installments
+	 */
+	public void setInstallments(int installments) {
+		this.installments = installments;
+	}
+
+	/**
+	 * Gets the observations.
+	 *
+	 * @return the observations
+	 */
+	public String getObservations() {
+		return observations;
+	}
+
+	/**
+	 * Sets the observations.
+	 *
+	 * @param observations
+	 *            the new observations
+	 */
+	public void setObservations(String observations) {
+		this.observations = observations;
+	}
+
+
+
+	public List<Account.roles> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(List<Account.roles> roles) {
+		this.roles = roles;
+	}
+
+	/**
+	 * Gets the role.
+	 *
+	 * @return the role
+	 */
+	public String getRole() {
+		return role;
+	}
+
+	/**
+	 * Sets the role.
+	 *
+	 * @param role
+	 *            the new role
+	 */
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	/**
+	 * Checks if is on name.
+	 *
+	 * @return true, if is on name
+	 */
+	public boolean isOnName() {
+		return onName;
+	}
+
+	/**
+	 * Sets the on name.
+	 *
+	 * @param onName
+	 *            the new on name
+	 */
+	public void setOnName(boolean onName) {
+		this.onName = onName;
+	}
+
+	/**
+	 * Checks if is on dni.
+	 *
+	 * @return true, if is on dni
+	 */
+	public boolean isOnDni() {
+		return onDni;
+	}
+
+	/**
+	 * Sets the on dni.
+	 *
+	 * @param onDni
+	 *            the new on dni
+	 */
+	public void setOnDni(boolean onDni) {
+		this.onDni = onDni;
+	}
+
+	/**
+	 * Checks if is on address.
+	 *
+	 * @return true, if is on address
+	 */
+	public boolean isOnAddress() {
+		return onAddress;
+	}
+
+	/**
+	 * Sets the on address.
+	 *
+	 * @param onAddress
+	 *            the new on address
+	 */
+	public void setOnAddress(boolean onAddress) {
+		this.onAddress = onAddress;
+	}
+
+	/**
+	 * Checks if is on login.
+	 *
+	 * @return true, if is on login
+	 */
+	public boolean isOnLogin() {
+		return onLogin;
+	}
+
+	/**
+	 * Sets the on login.
+	 *
+	 * @param onLogin
+	 *            the new on login
+	 */
+	public void setOnLogin(boolean onLogin) {
+		this.onLogin = onLogin;
+	}
+
+	/**
+	 * Checks if is on email.
+	 *
+	 * @return true, if is on email
+	 */
+	public boolean isOnEmail() {
+		return onEmail;
+	}
+
+	/**
+	 * Sets the on email.
+	 *
+	 * @param onEmail
+	 *            the new on email
+	 */
+	public void setOnEmail(boolean onEmail) {
+		this.onEmail = onEmail;
+	}
+
+	/**
+	 * Checks if is on phone.
+	 *
+	 * @return true, if is on phone
+	 */
+	public boolean isOnPhone() {
+		return onPhone;
+	}
+
+	/**
+	 * Sets the on phone.
+	 *
+	 * @param onPhone
+	 *            the new on phone
+	 */
+	public void setOnPhone(boolean onPhone) {
+		this.onPhone = onPhone;
+	}
+
+	/**
+	 * Checks if is on mobile.
+	 *
+	 * @return true, if is on mobile
+	 */
+	public boolean isOnMobile() {
+		return onMobile;
+	}
+
+	/**
+	 * Sets the on mobile.
+	 *
+	 * @param onMobile
+	 *            the new on mobile
+	 */
+	public void setOnMobile(boolean onMobile) {
+		this.onMobile = onMobile;
+	}
+
+	/**
+	 * Checks if is onprogram name.
+	 *
+	 * @return true, if is onprogram name
+	 */
+	public boolean isOnProgramName() {
+		return onProgramName;
+	}
+
+	/**
+	 * Sets the onprogram name.
+	 *
+	 * @param onprogramName
+	 *            the new onprogram name
+	 */
+	public void setOnProgramName(boolean onProgramName) {
+		this.onProgramName = onProgramName;
+	}
+
+	/**
+	 * Checks if is on student.
+	 *
+	 * @return true, if is on student
+	 */
+	public boolean isOnStudent() {
+		return onStudent;
+	}
+
+	/**
+	 * Sets the on student.
+	 *
+	 * @param onStudent
+	 *            the new on student
+	 */
+	public void setOnStudent(boolean onStudent) {
+		this.onStudent = onStudent;
+	}
+
+	/**
+	 * Checks if is on date birth.
+	 *
+	 * @return true, if is on date birth
+	 */
+	public boolean isOnDateBirth() {
+		return onDateBirth;
+	}
+
+	/**
+	 * Sets the on date birth.
+	 *
+	 * @param onDateBirth
+	 *            the new on date birth
+	 */
+	public void setOnDateBirth(boolean onDateBirth) {
+		this.onDateBirth = onDateBirth;
+	}
+
+	/**
+	 * Checks if is on password.
+	 *
+	 * @return true, if is on password
+	 */
+	public boolean isOnPassword() {
+		return onPassword;
+	}
+
+	/**
+	 * Sets the on password.
+	 *
+	 * @param onPassword
+	 *            the new on password
+	 */
+	public void setOnPassword(boolean onPassword) {
+		this.onPassword = onPassword;
+	}
+
+	/**
+	 * Checks if is on account type.
+	 *
+	 * @return true, if is on account type
+	 */
+	public boolean isOnAccountType() {
+		return onAccountType;
+	}
+
+	/**
+	 * Sets the on account type.
+	 *
+	 * @param onAccountType
+	 *            the new on account type
+	 */
+	public void setOnAccountType(boolean onAccountType) {
+		this.onAccountType = onAccountType;
+	}
+
+	/**
+	 * Checks if is on method payment.
+	 *
+	 * @return true, if is on method payment
+	 */
+	public boolean isOnMethodPayment() {
+		return onMethodPayment;
+	}
+
+	/**
+	 * Sets the on method payment.
+	 *
+	 * @param onMethodPayment
+	 *            the new on method payment
+	 */
+	public void setOnMethodPayment(boolean onMethodPayment) {
+		this.onMethodPayment = onMethodPayment;
+	}
+
+	/**
+	 * Checks if is on installments.
+	 *
+	 * @return true, if is on installments
+	 */
+	public boolean isOnInstallments() {
+		return onInstallments;
+	}
+
+	/**
+	 * Sets the on installments.
+	 *
+	 * @param onInstallments
+	 *            the new on installments
+	 */
+	public void setOnInstallments(boolean onInstallments) {
+		this.onInstallments = onInstallments;
+	}
+
+	/**
+	 * Checks if is on observations.
+	 *
+	 * @return true, if is on observations
+	 */
+	public boolean isOnObservations() {
+		return onObservations;
+	}
+
+	/**
+	 * Sets the on observations.
+	 *
+	 * @param onObservations
+	 *            the new on observations
+	 */
+	public void setOnObservations(boolean onObservations) {
+		this.onObservations = onObservations;
+	}
+
+	/**
+	 * Checks if is on role.
+	 *
+	 * @return true, if is on role
+	 */
+	public boolean isOnRole() {
+		return onRole;
+	}
+
+	/**
+	 * Sets the on role.
+	 *
+	 * @param onRole
+	 *            the new on role
+	 */
+	public void setOnRole(boolean onRole) {
+		this.onRole = onRole;
+	}
+
 }
